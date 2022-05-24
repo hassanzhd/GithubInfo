e9966cd0bac7556fa04bfeabc0d5985cfea22058
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2022-05-24 19:51:36 +0500

    updated api/ and workflows/

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,22 @@
 import express from "express";
+import fetch from "node-fetch";
+import { GitHubUserService } from "./services/GithubUserService";
 
+const PORT = 5000;
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json({ message: "HELLO!" });
+app.get("/:username", async (request, response) => {
+  const { username } = request.params;
+
+  try {
+    const service = GitHubUserService.getInstance();
+    const data = await service.getUserProfile(username);
+    response.json(data);
+  } catch (error) {
+    response.status(404).json({ message: error.message });
+  }
 });
 
-app.listen(5000, () => {
-  console.log("SERVER LISTENING");
+app.listen(PORT, () => {
+  console.log(`SERVER LISTENING ON PORT ${PORT}`);
 });
---
+++ api/src/services/GithubUserService.js
@@ -0,0 +1,30 @@
+import fetch from "node-fetch";
+
+export class GitHubUserService {
+  constructor() {
+    this.instance = undefined;
+  }
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new GitHubUserService();
+    }
+
+    return this.instance;
+  }
+
+  async getUserProfile(__username) {
+    const githubResponse = await fetch(
+      `https://api.github.com/users/${__username}`,
+      { "User-Agent": "hassanzhd" }
+    );
+
+    const data = await githubResponse.json();
+
+    if (!githubResponse.ok) {
+      throw new Error(data.message);
+    }
+
+    return data;
+  }
+}
